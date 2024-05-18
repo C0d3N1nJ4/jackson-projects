d47611f14dc48e62dcf4cc4a576a1b8cd8d771f4
naiomi <Naiomi.Naidoo@icloud.com>
2024-05-18 22:57:06 +0200

    Initial project commit

---
+++ json-service/src/test/java/com/naiomi/jsonservice/JsonServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.naiomi.jsonservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsonServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
